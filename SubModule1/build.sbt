5b719cd6d1bacdb90c019d9e8737ae136e03582b
eifaookfue <s7.nakamura@gmail.com>
2020-04-18 16:06:24 +0900

    二回目

--- SubModule1/build.sbt
+++ SubModule1/build.sbt
@@ -1,3 +1,3 @@
-name := "submodule1"
+name := "Submodule1"
 
-version := "0.1"
+version := "0.2"
---
+++ SubModule1/src/main/scala/jp/co/nri/nefs/tool/submodule1/SubModule.scala
@@ -0,0 +1,7 @@
+package jp.co.nri.nefs.tool.submodule1
+
+object SubModule {
+  def main(args: Array[String]): Unit = {
+    println("Hello World!")
+  }
+}
